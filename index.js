4458c71b8c6123c9ca4891715908464481ceff9a
ahmedkh45 <ahmed.kh11333@gmail.com>
2022-07-24 23:52:47 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+    (window).scroll(function(){
+        var scroll = (window).scrollTop();
+        if (scroll > 2) {
+          (".navbar").css("background" , "rgb(55, 192, 23)");
+        }
+        else{
+            (".black").css("background" , "rgb(55, 192, 3)");  	
+        }
+    })
+  
